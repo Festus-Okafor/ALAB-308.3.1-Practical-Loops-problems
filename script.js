04a09f2708258803ec2c5717a1a6bbf0944cd7a4
mexie07 <okaforfestus07@gmail.com>
2025-05-02 12:52:36 -0400

    solution to quest 3 added

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+//Part 1: Fizz Buzz
+
 for (let i = 1; i <= 100; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
     console.log(`Fizz Buzz`);
@@ -11,7 +15,7 @@ for (let i = 1; i <= 100; i++) {
 }
 
 console.log(`....................`);
-
+//Part 2: Prime Time
 let number = 14796;
 
 let isPrime = true;
@@ -29,3 +33,52 @@ if (isPrime) {
 } else {
   console.log(number + " is not a prime");
 }
+
+
+
+
+
+//part 3
+//Feeling loopy Problem
+//new line is used as a escape line
+//
+let csvData = ["ID","Name","Occupation",
+"Age","\n","42","Bruce","Knight","41","\n","57",
+"Bob","Fry","Cook","19","\n","63",
+"Blaine","Quiz","Master",
+"58","\n","98","Bill","Doctor’s","Assistant","26"]
+
+for (let i =0; i<csvData.length; i++){
+
+    if(csvData[i]==ln){
+       console.log(`cell1`);
+    }
+}
+
+
+
+
+console.log(`....................`);
+
+let mixString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41,\n,57,Bob,Fry,Cook,19,\n,63,Blaine,Quiz-Mster,58,\n,98,Bill,Doctor’s-Assistant,26"
+//let Data = "Index, Mass (kg), Spring 1(m), Spring 2(m)" 
+
+
+   let cell1 = '';
+   let cell2 = '';
+   let cell3 = '';
+   let cell4 = '';
+
+
+for( let i = 0; i < mixString.length; i++){
+ let char = mixString[i]
+     cell1 += char;
+     if(char === '\n'){
+        continue;
+   }
+}
+   console.log(cell1);
+  
+    
+
+ 
